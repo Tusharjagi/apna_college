07c7e91ece9dc3dc206b5bb7c59b8b6fbf2350ea
Tushar Jagi <tushar.j@fnp.com>
2024-09-16 02:32:54 +0530

    Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import List from "@/components/list";
+import { Grid, Typography } from "@mui/joy";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <Grid className="mx-6 my-6 md:mx-16 lg:mx-16 md:my-12 lg:my-12">
+      <Typography
+        level="h1"
+        className="hover:underline hover:text-primary transition-all duration-300 ease-in-out"
+      >
+        Data Structure & Algorithms
+      </Typography>
+      <List />
+    </Grid>
+  );
 }
