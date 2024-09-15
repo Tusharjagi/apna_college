7806c1afa76ad76fe7f8136e6e807ba77c67d418
Tushar Jagi <tushar.j@fnp.com>
2024-09-15 14:21:09 +0530

    Header added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "../styles/globals.css";
+import Header from "@/components/header";
 
 const geistSans = localFont({
   src: "../public/fonts/GeistVF.woff",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header />
         {children}
       </body>
     </html>
---
+++ src/components/header.tsx
@@ -0,0 +1,18 @@
+import { Grid } from "@mui/joy";
+import Button from "@mui/joy/Button";
+
+export default function Header() {
+  return (
+    <Grid className="w-full h-[5rem] md:h-14 lg:h-16 flex justify-between items-center px-5 md:px-16 lg:px-16 shadow-md">
+      <Grid className=" text-nowrap font-bold">Apna College</Grid>
+      <Grid className="gap-5 md:gap-8 lg:gap-8 flex">
+        <Button variant="outlined" className="text-primary">
+          Log in
+        </Button>
+        <Button variant="outlined" className="text-primary">
+          Sign up
+        </Button>
+      </Grid>
+    </Grid>
+  );
+}
