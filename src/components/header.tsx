8b9fde491942d807b90538b095fe0275c6596662
Tushar Jagi <tushar.j@fnp.com>
2024-09-16 04:24:10 +0530

    handle routes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,39 @@
+"use client";
+
 import { Grid } from "@mui/joy";
 import Button from "@mui/joy/Button";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Header() {
+  const router = useRouter();
+
+  const handleLogin = () => {
+    router.push("/user/login");
+  };
+
+  const handleSignUp = () => {
+    router.push("/user/signup");
+  };
+
   return (
     <Grid className="w-full h-[5rem] md:h-14 lg:h-16 flex justify-between items-center px-5 md:px-16 lg:px-16 shadow-md">
       <Grid className=" text-nowrap font-bold">
         <Link href="/#">Apna College</Link>
       </Grid>
       <Grid className="gap-5 md:gap-8 lg:gap-8 flex">
-        <Button variant="outlined" className="text-primary">
+        <Button
+          variant="outlined"
+          className="text-primary"
+          onClick={handleLogin}
+        >
           Log in
         </Button>
-        <Button variant="outlined" className="text-primary">
+        <Button
+          variant="outlined"
+          className="text-primary"
+          onClick={handleSignUp}
+        >
           Sign up
         </Button>
       </Grid>
