259ebdd9f9a8bb486be99670a8f172fdb363d663
Tushar Jagi <tushar.j@fnp.com>
2024-09-16 02:33:24 +0530

    sub lists

---
+++ src/components/sub-list.tsx
@@ -0,0 +1,41 @@
+import { Grid } from "@mui/joy";
+import Link from "next/link";
+
+type SubListItem = {
+  subTitle: string;
+  id: string;
+};
+
+type SubListProps = {
+  list: {
+    subList: SubListItem[] | string;
+    title: string;
+  };
+};
+
+export default function SubList({ list }: SubListProps) {
+  if (typeof list.subList === "string") {
+    return <Grid className="p-2">{list.subList}</Grid>;
+  }
+
+  const getAlphabet = (index: number) => {
+    if (index > 25) {
+      return "*";
+    }
+    return String.fromCharCode(65 + index).toLowerCase();
+  };
+
+  return (
+    <Grid className="p-2">
+      {list.subList.map((item, index) => {
+        return (
+          <Grid key={index} className="ml-2 mb-1">
+            <Link href={item.id}>
+              {getAlphabet(index)}. {item.subTitle}
+            </Link>
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
+}
