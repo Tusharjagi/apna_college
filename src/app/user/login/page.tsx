e924b29f3afe2fd97373a13cc744f77cda643ecd
Tushar Jagi <tushar.j@fnp.com>
2024-09-16 11:31:46 +0530

    login and sign up pages

--- src/app/user/login/page.tsx
+++ src/app/user/login/page.tsx
@@ -1,3 +1,69 @@
-export default function Login() {
-  return <h1>Login page</h1>;
-}
+"use client";
+
+import { Box, Button, Container, Input, Typography } from "@mui/joy";
+import React, { useState } from "react";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    console.log("Logging in with:", username, password);
+  };
+
+  return (
+    <Container maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+        className="gap-6"
+      >
+        <Typography level="h4" component="h1">
+          Login
+        </Typography>
+        <Box
+          component="form"
+          sx={{ mt: 1, width: "100%" }}
+          noValidate
+          className="grid gap-8"
+        >
+          <Input
+            placeholder="Name"
+            autoFocus
+            required
+            autoComplete="current-name"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            color="success"
+            variant="outlined"
+          />
+          <Input
+            color="success"
+            variant="outlined"
+            autoComplete="current-password"
+            placeholder="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="solid"
+            onClick={handleLogin}
+            color="success"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Login
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default Login;
